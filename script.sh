fca7cf8cd3a70c7e6466d1923183e92d253bc787
Slavapat5 <erikpompov@gmail.com>
2026-02-03 11:27:18 +0000

    Set _2 files to rwx for all users

--- script.sh
+++ script.sh
@@ -2,8 +2,9 @@
 #!/usr/bin/env bash
 
 set -e
-rm -rf fol_1 fol_2
+umask 077
 
+rm -rf fol_1 fol_2
 mkdir -p fol_1 fol_2
 
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
@@ -11,3 +12,5 @@ touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
 chmod 600 fol_1/*_1.txt fol_1/*_3.txt fol_2/*_1.txt fol_2/*_3.txt
+
+chmod 777 fol_1/*_2.txt fol_2/*_2.txt
